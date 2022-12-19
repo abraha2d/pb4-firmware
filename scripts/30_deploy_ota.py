836bf90241c497eab38228b18a806aef9bfefd73
Kevin Abraham <abraha2d@users.noreply.github.com>
2022-12-18 20:01:22 -0500

    Rewrite, dedupe OTA code

--- scripts/30_deploy_ota.py
+++ scripts/30_deploy_ota.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 from binascii import hexlify
 from hashlib import sha256
+from math import ceil
 from sys import argv
 
 import paho.mqtt.client as mqtt
@@ -11,27 +14,38 @@ OK_TOPIC = "/pb4/devices/{}/ota/{}/ok"
 OTA_TYPE = None
 OTA_DEVICE = None
 
-OTA_DATA = None
+BLOCK_SIZE = 4096  # hard to get from target device without a lot of work
+CHUNK_SIZE = 65536  # max 66977
+OTA_CHUNKS = None
 OTA_HASH = None
+
+
+def progress():
+    while True:
+        yield from '-\\|/'
+
+
+PROGRESS = progress()
 
 
 # noinspection PyUnusedLocal
 def on_connect(client, userdata, flags, rc):
-    global OTA_DATA
-
     client.on_connect = None
     print(" connected.")
-    print("Publishing data...", end="", flush=True)
-    client.publish(DATA_TOPIC.format(OTA_DEVICE, OTA_TYPE), OTA_DATA, qos=2)
-    OTA_DATA = None
+    print("Publishing data...  ", end="", flush=True)
+    on_publish(client, None, None)
 
 
 # noinspection PyUnusedLocal
 def on_publish(client, userdata, mid):
-    global OTA_HASH
+    global OTA_CHUNKS, OTA_HASH
 
-    if OTA_HASH is not None:
-        print(" done.")
+    if len(OTA_CHUNKS) > 0:
+        print(f"\b{next(PROGRESS)}", end="", flush=True)
+        client.publish(DATA_TOPIC.format(OTA_DEVICE, OTA_TYPE), OTA_CHUNKS[0], qos=2)
+        OTA_CHUNKS = OTA_CHUNKS[1:]
+    elif OTA_HASH is not None:
+        print("\bdone.")
         print("Publishing hash...", end="", flush=True)
         client.publish(HASH_TOPIC.format(OTA_DEVICE, OTA_TYPE), OTA_HASH, qos=2)
         OTA_HASH = None
@@ -58,12 +72,12 @@ def on_message(client, userdata, message):
 
 
 def main():
-    global OTA_DATA, OTA_DEVICE, OTA_HASH, OTA_TYPE
+    global CHUNK_SIZE, OTA_CHUNKS, OTA_DEVICE, OTA_HASH, OTA_TYPE
 
     OTA_TYPE = argv[1]
     OTA_DEVICE = argv[2]
 
-    bin_file = f"build/{OTA_TYPE}.bin"
+    bin_file = f"build/{'micropython' if OTA_TYPE == 'fw' else OTA_TYPE}.bin"
 
     client = mqtt.Client()
     client.on_connect = on_connect
@@ -73,13 +87,23 @@ def main():
 
     print(f"Reading {bin_file}...", end="", flush=True)
     with open(bin_file, "rb") as payload_file:
-        OTA_DATA = payload_file.read()
-        print(f" done. {len(OTA_DATA)} bytes.")
+        raw_data = payload_file.read()
+        print(f" done. {len(raw_data)} bytes.")
+
+        print(f"Extending to nearest {ceil(BLOCK_SIZE / 1024)}K block...", end="", flush=True)
+        pad_length = ceil(len(raw_data) / BLOCK_SIZE) * BLOCK_SIZE
+        ota_data = raw_data.ljust(pad_length, b'\xFF')
+        print(f" done. {ceil(pad_length / 1024)}K bytes.")
 
         print("Hashing...", end="", flush=True)
-        OTA_HASH = sha256(OTA_DATA).digest()
+        OTA_HASH = sha256(ota_data).digest()
         print(" done.")
         print(f"SHA256: {hexlify(OTA_HASH).decode()}")
+
+        print("Chunking...", end="", flush=True)
+        OTA_CHUNKS = [ota_data[i:i + CHUNK_SIZE] for i in range(0, len(ota_data), CHUNK_SIZE)]
+        print(" done.")
+        print(f"{len(OTA_CHUNKS)} chunks. Max chunk size: {ceil(CHUNK_SIZE / 1024)}K bytes.")
 
     print("Connecting to MQTT broker...", end="", flush=True)
     client.connect("pb4_control.local", keepalive=1)
--- src/ota.py
+++ src/ota.py
@@ -18,136 +18,101 @@ from config import (
 )
 from upy_platform import status
 
-FW_DATA = bytearray()
-APP_DATA = bytearray()
+STAGING = Partition.find(Partition.TYPE_DATA, label='staging')[0]
+BLOCK_SIZE = STAGING.ioctl(5, None)
+STAGING_OFFSET = 0
 
 
 async def setup_ota_subscriptions(client):
     await client.subscribe(
-        (MQTT_TOPIC_OTA_FW_DATA, 2, recv_fw_data),
+        (MQTT_TOPIC_OTA_FW_DATA, 2, recv_data),
         (MQTT_TOPIC_OTA_FW_HASH, 2, recv_fw_hash),
-        (MQTT_TOPIC_OTA_APP_DATA, 2, recv_app_data),
+        (MQTT_TOPIC_OTA_APP_DATA, 2, recv_data),
         (MQTT_TOPIC_OTA_APP_HASH, 2, recv_app_hash),
     )
 
 
 # noinspection PyUnusedLocal
-async def recv_fw_data(client, topic, data, retained):
-    assert topic == MQTT_TOPIC_OTA_FW_DATA
+async def recv_data(client, topic, data, retained):
+    global STAGING_OFFSET
     if retained:
         return
 
-    global FW_DATA
-    FW_DATA = data
+    STAGING.writeblocks(STAGING_OFFSET, data)
+    STAGING_OFFSET += ceil(len(data) / BLOCK_SIZE)
 
-    print(f"ota.recv_fw_data: Received {len(data)} bytes.")
+    print(f"ota.recv_data: Received {ceil(STAGING_OFFSET * BLOCK_SIZE / 1024)}K bytes (+{ceil(len(data) / 1024)}K).")
     status.app_state = status.APP_UPGRADING
+
+
+async def recv_hash(client, data, retained, to_update, ok_topic, use_ota=False):
+    global STAGING_OFFSET
+    if retained:
+        return
+
+    hasher = sha256()
+    ota_chunk = bytearray(BLOCK_SIZE)
+    for i in range(STAGING_OFFSET):
+        await sleep_ms(0)
+        STAGING.readblocks(i, ota_chunk)
+        hasher.update(ota_chunk)
+    ota_hash = hasher.digest()
+
+    if ota_hash == data:
+        print(f"ota.recv_hash: Validated hash: SHA256:{hexlify(ota_hash).decode()}")
+        part_label = to_update.info()[4]
+
+        try:
+            print(f"ota.recv_hash: Writing update to partition '{part_label}'...")
+            print(f"ota.recv_hash: INFO block size {BLOCK_SIZE} bytes => {STAGING_OFFSET} blocks")
+
+            if use_ota:
+                handle = to_update.ota_begin(STAGING_OFFSET * BLOCK_SIZE)
+
+            for i in range(STAGING_OFFSET):
+                await sleep_ms(0)
+                STAGING.readblocks(i, ota_chunk)
+                if use_ota:
+                    # noinspection PyUnboundLocalVariable
+                    to_update.ota_write(handle, ota_chunk)
+                else:
+                    to_update.writeblocks(i, ota_chunk)
+
+            if use_ota:
+                to_update.ota_end(handle)
+                print(f"ota.recv_hash: Success. Choosing '{part_label}' for next boot...")
+                to_update.set_boot()
+
+            print(f"ota.recv_hash: Done. Communicating status to broker...")
+            await client.publish(ok_topic, "1", 1)
+
+            print(f"ota.recv_hash: OK. Rebooting in 5 seconds...")
+            status.app_state = status.APP_SHUTDOWN
+            await sleep_ms(5000)
+            status.write(status.BLACK)
+            reset()
+            return
+        except OSError as e:
+            print_exception(e)
+    else:
+        print(f"ota.recv_hash: Hash invalid! " +
+              f"actual (SHA256:{hexlify(ota_hash).decode()}) != expected (SHA256:{hexlify(data).decode()})")
+
+    print(f"ota.recv_hash: Communicating failure to broker...")
+    await client.publish(ok_topic, "0", 1)
+    STAGING_OFFSET = 0
+    status.app_state = status.APP_ERROR
 
 
 async def recv_fw_hash(client, topic, data, retained):
     assert topic == MQTT_TOPIC_OTA_FW_HASH
-    if retained:
-        return
-
-    global FW_DATA
-    fw_hash = sha256(FW_DATA).digest()
-
-    if fw_hash == data:
-        print(f"ota.recv_fw_hash: Validated hash: SHA256:{hexlify(fw_hash).decode()}")
-
-        to_update = Partition(Partition.RUNNING).get_next_update()
-        part_label = to_update.info()[4]
-
-        try:
-            print(f"ota.recv_fw_hash: Writing update to partition '{part_label}'...")
-
-            block_size = to_update.ioctl(5, None)
-            block_num = ceil(len(FW_DATA) / block_size)
-            print(f"ota.recv_app_hash: INFO block size {block_size} bytes => {block_num} blocks")
-
-            handle = to_update.ota_begin(len(FW_DATA))
-            for i in range(block_num):
-                await sleep_ms(0)
-                start, end = i * block_size, (i + 1) * block_size
-                to_update.ota_write(handle, FW_DATA[start:end])
-            to_update.ota_end(handle)
-
-            print(f"ota.recv_fw_hash: Success. Choosing '{part_label}' for next boot...")
-            to_update.set_boot()
-
-            print(f"ota.recv_fw_hash: Done. Communicating status to broker...")
-            await client.publish(MQTT_TOPIC_OTA_FW_OK, "1", 1)
-
-            print(f"ota.recv_fw_hash: OK. Rebooting in 5 seconds...")
-            status.app_state = status.APP_SHUTDOWN
-            await sleep_ms(5000)
-            status.write(status.BLACK)
-            reset()
-            return
-        except OSError as e:
-            print_exception(e)
-            status.app_state = status.APP_ERROR
-    else:
-        print(f"ota.recv_fw_hash: Hash invalid! " +
-              f"actual (SHA256:{hexlify(fw_hash).decode()}) != expected (SHA256:{hexlify(data).decode()})")
-
-    print(f"ota.recv_fw_hash: Communicating failure to broker...")
-    await client.publish(MQTT_TOPIC_OTA_FW_OK, "0", 1)
-    status.app_state = status.APP_ERROR
-
-
-# noinspection PyUnusedLocal
-async def recv_app_data(client, topic, data, retained):
-    assert topic == MQTT_TOPIC_OTA_APP_DATA
-    if retained:
-        return
-
-    global APP_DATA
-    APP_DATA = data
-    print(f"ota.recv_app_data: Received {len(data)} bytes.")
-    status.app_state = status.APP_UPGRADING
+    to_update = Partition(Partition.RUNNING).get_next_update()
+    ok_topic = MQTT_TOPIC_OTA_FW_OK
+    await recv_hash(client, data, retained, to_update, ok_topic, use_ota=True)
 
 
 async def recv_app_hash(client, topic, data, retained):
     assert topic == MQTT_TOPIC_OTA_APP_HASH
-    if retained:
-        return
-
-    global APP_DATA
-    app_hash = sha256(APP_DATA).digest()
-
-    if app_hash == data:
-        print(f"ota.recv_app_hash: Validated hash: SHA256:{hexlify(app_hash).decode()}")
-
-        to_update = Partition('vfs')
-        part_label = to_update.info()[4]
-
-        try:
-            print(f"ota.recv_app_hash: Writing update to partition '{part_label}'...")
-            block_size = to_update.ioctl(5, None)
-            block_num = ceil(len(APP_DATA) / block_size)
-            print(f"ota.recv_app_hash: INFO block size {block_size} bytes => {block_num} blocks")
-            for i in range(block_num):
-                await sleep_ms(0)
-                start, end = i * block_size, (i + 1) * block_size
-                to_update.writeblocks(i, APP_DATA[start:end])
-
-            print(f"ota.recv_app_hash: Done. Communicating status to broker...")
-            await client.publish(MQTT_TOPIC_OTA_APP_OK, "1", 1)
-
-            print(f"ota.recv_app_hash: OK. Rebooting in 5 seconds...")
-            status.app_state = status.APP_SHUTDOWN
-            await sleep_ms(5000)
-            status.write(status.BLACK)
-            reset()
-            return
-        except OSError as e:
-            print_exception(e)
-            status.app_state = status.APP_ERROR
-    else:
-        print(f"ota.recv_app_hash: Hash invalid! " +
-              f"actual ({hexlify(app_hash).decode()}) != expected ({hexlify(data).decode()})")
-
-    print(f"ota.recv_app_hash: Communicating failure to broker...")
-    await client.publish(MQTT_TOPIC_OTA_APP_OK, "0", 1)
-    status.app_state = status.APP_ERROR
+    to_update = Partition.find(Partition.TYPE_DATA, label='staging')[0]
+    ok_topic = MQTT_TOPIC_OTA_APP_OK
+    await recv_hash(client, data, retained, to_update, ok_topic, use_ota=True)
